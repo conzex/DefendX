ab330a3cd4d826c0c51add597c4a9cd2db947e7b
SUMIT KUMAWAT <74807141+sumit-kumawat@users.noreply.github.com>
2025-05-31 17:32:33 +0530

    Update linux-agent.sh

--- linux-agent.sh
+++ linux-agent.sh
@@ -9,7 +9,7 @@ sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
 # Download GPG key
 echo "Fetching GPG key..."
-wget -O /usr/share/keyrings/defendx.gpg https://packages.defendx.io/linux/defendx-agent.gpg.key
+curl -fsSL https://packages.defendx.io/linux/defendx-agent.gpg.key | gpg --dearmor -o /usr/share/keyrings/defendx.gpg
 
 # Add APT source list
 echo "Adding APT repository..."
